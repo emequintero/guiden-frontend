5fa39e9f925ea3336ed7e54c0d0f27268766fe58
Matthias Quintero <matthiasquintero@gmail.com>
2020-04-30 16:45:11 -0600

    full animation of djikstra with walls and start/node re-placement

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AlgoService } from 'src/app/services/algo.service';
 import { DependenciesService } from 'src/app/services/dependencies.service';
-import {Node} from '../../models/node';
+import { Node } from '../../models/node';
 
 @Component({
   selector: 'app-nav',
@@ -9,56 +9,60 @@ import {Node} from '../../models/node';
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent implements OnInit {
-  algoDescription:string = "";
-  algorithms:string[] = ["Dijkstra", "A*"];
-  selectedAlgo:string = "Dijkstra";
-  constructor(private algoService:AlgoService, private dependenciesService:DependenciesService) { }
+  algoDescription: string = "";
+  algorithms: string[] = ["Dijkstra", "A*"];
+  selectedAlgo: string = "Dijkstra";
+  constructor(private algoService: AlgoService, private dependenciesService: DependenciesService) { }
 
   ngOnInit(): void {
   }
 
-  findPath(){
-    this.algoService.findPath("dijkstra", 
-    this.dependenciesService.getStart(), this.dependenciesService.getFinish(), 
-    this.dependenciesService.getRows(), this.dependenciesService.getColumns(), 
-    this.dependenciesService.getWalls())
-    .subscribe(algoResponse=>{
-      //set visited nodes in order
-      let visitedNodesInOrder:Node[] = algoResponse["visitedNodesInOrder"].map(node=>{
-        let currentNode:Node = new Node(node["row"],node["column"]);
-        currentNode.distance = node["distance"];
-        currentNode.isFinish = node["finish"];
-        currentNode.isStart = node["start"];
-        currentNode.isWall = node["wall"];
-        currentNode.isVisited = true;
-        return currentNode;
+  findPath() {
+    this.algoService.findPath("dijkstra",
+      this.dependenciesService.getStart(), this.dependenciesService.getFinish(),
+      this.dependenciesService.getRows(), this.dependenciesService.getColumns(),
+      this.dependenciesService.getWalls())
+      .subscribe(algoResponse => {
+        //set visited nodes in order
+        let visitedNodesInOrder: Node[] = algoResponse["visitedNodesInOrder"].map(node => {
+          let currentNode: Node = new Node(node["row"], node["column"]);
+          currentNode.distance = node["distance"];
+          currentNode.isFinish = node["finish"];
+          currentNode.isStart = node["start"];
+          currentNode.isWall = node["wall"];
+          currentNode.isVisited = true;
+          return currentNode;
+        });
+        this.dependenciesService.setVisitedNodesInOrder(visitedNodesInOrder);
+        //path (final path animation)
+        let path = algoResponse["path"].map(node => {
+          let currentNode: Node = new Node(node["row"], node["column"]);
+          currentNode.distance = node["distance"];
+          currentNode.isFinish = node["finish"];
+          currentNode.isStart = node["start"];
+          currentNode.isWall = node["wall"];
+          currentNode.isInShortestPath = true;
+          return currentNode;
+        });
+        this.dependenciesService.setPath(path);
+        //animate algorithm
+        this.dependenciesService.animate();
       });
-      this.dependenciesService.setVisitedNodesInOrder(visitedNodesInOrder);
-      //path (final path animation)
-      let path = algoResponse["path"].map(node=>{
-        let currentNode:Node = new Node(node["row"],node["column"]);
-        currentNode.distance = node["distance"];
-        currentNode.isFinish = node["finish"];
-        currentNode.isStart = node["start"];
-        currentNode.isWall = node["wall"];
-        currentNode.isInShortestPath = true;
-        return currentNode;
-      });
-      this.dependenciesService.setPath(path);
-      //animate algorithm
-      this.dependenciesService.animate();
-    });
   }
 
-  resetGraph(){
+  resetGraph() {
     //reset graph
     this.dependenciesService.resetGraph();
     //reset walls
     this.dependenciesService.resetWalls();
-  }
+    //reset starting node
+    this.dependenciesService.setStart([5, 11]);
+    //reset finish node
+    this.dependenciesService.setFinish([5, 28]);
+}
 
-  setAlgorithm(){
+setAlgorithm(){
 
-  }
+}
 
 }
